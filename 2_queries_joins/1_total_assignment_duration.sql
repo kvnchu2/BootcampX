ef20ca015e1215b80ccb1f9baf325231a7f6c024
Kevin Chu <kvnchu2@gmail.com>
2021-01-05 01:31:19 +0000

    add joins queries

---
+++ 2_queries_joins/1_total_assignment_duration.sql
@@ -0,0 +1,4 @@
+SELECT sum(duration) AS total_duration
+FROM students
+JOIN assignment_submissions ON students.id = student_id
+WHERE name = 'Ibrahim Schimmel';
---
+++ 2_queries_joins/2_cohort_total_assignment_duration.sql
@@ -0,0 +1,5 @@
+SELECT sum(duration) AS total_duration
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+JOIN assignment_submissions ON students.id = student_id 
+WHERE cohorts.name = 'FEB12';
